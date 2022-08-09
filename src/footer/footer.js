5e897cb01fff7ebff2377ff88db3a28d13a54752
Macbook Pro 2016 <macbookpro2016@Macbooks-MacBook-Pro.local>
2022-08-09 13:49:05 -0400

    Add Header & Footer with scss Files

---
+++ src/footer/footer.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import "./footer.scss";
+
+class Footer extends Component {
+  render() {
+    return <span className="footer-container">I am footer</span>;
+  }
+}
+export default Footer;
