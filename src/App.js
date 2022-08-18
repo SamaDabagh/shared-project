c0bd3aec1bcf3f6b9cc4c103a712e65b7d9674c3
Mohammad Hossein Fallah Tafti <hosseinfallaht@gmail.com>
2022-08-18 14:04:47 -0400

    explnaitions for landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import ProductInformationPage from "./pages/product-information-page/product-information-page";
+import Header from './header/header'
+import Footer from "./footer/footer";
 
 function App() {
   return (
     <div className="App">
-      <ProductInformationPage code="5002" />
+      <Header />
+        {/* <LandingPage /> */}
+        <ProductInformationPage code="5002" />
+      <Footer />
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+LandingPage
+
+// temparray = [
+//   {
+//     name: test1,
+//     price: 11,
+//     imag: 'url/qhgjhqgdjfhg'
+//   },
+//   {
+//     name: test2,
+//     price: 12,
+//     imag: 'url/qhgjhqgdjfhg'
+//   },
+//   {
+//     name: test,
+//     price: 13,
+//     imag: 'url/qhgjhqgdjfhg'
+//   }
+// ];
+
+temp array should be fetched
+
+Map.
+<productItemComponent temparray[i]>
