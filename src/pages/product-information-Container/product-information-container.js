26c00a9baca87b6be069477202621956fc90e567
SamaDabagh <103628925+SamaDabagh@users.noreply.github.com>
2022-08-18 12:27:19 -0400

    Add counter of Items input to PIC (#7)
    * Add counter input to PIC
    
    * modified  totalItemser-input and Add-to-cart to PIC
    
    * modified name of functions to handleTotalItemser  and handleAddToCart
    
    * difined  min value in input number
    
    * correct splling of handlerfunctions name

--- src/pages/product-information-Container/product-information-container.js
+++ src/pages/product-information-Container/product-information-container.js
@@ -2,20 +2,60 @@ import { Component } from "react";
 import "./product-information-container.scss";
 
 class ProductInformationContainer extends Component {
+  state = {
+    numberOfItems: 0,
+    countOfWantedItems: 0,
+  };
+
+  handleTotalItems = (event) => {
+    const { item } = this.props;
+
+    let countOfWantedItems = event.target.value;
+    this.setState({
+      countOfWantedItems,
+    });
+    console.log({ item, countOfWantedItems });
+    // return countOfWantedItems;
+  };
+  handleAddToCart = () => {
+    const { item } = this.props;
+    let { numberOfItems } = this.state;
+    ++numberOfItems;
+    this.setState({
+      numberOfItems: numberOfItems,
+    });
+
+    console.log({ item, numberOfItems });
+  };
+
   render() {
     const { price, name, pricePreFix, canBeSold } = this.props.product;
 
     return (
       <div className="product-Info-container">
-        <div className="font-container"> Name : {name}</div>
-        <div className="font-container">
-          Price({pricePreFix}) : {price}
+        <div>
+          <div className="font-container"> Name : {name}</div>
+          <div className="font-container">
+            Price({pricePreFix}) : {price}
+          </div>
+          {canBeSold ? (
+            <div className="font-container"> Available </div>
+          ) : (
+            <div> Unavailable </div>
+          )}
+          <div>
+            <input
+              className="counter"
+              type="number"
+              onChange={this.handleTotalItems}
+              placeholder="0"
+              min={0}
+            />
+            <button className="btn-addToCart" onClick={this.handleAddToCart}>
+              Add to cart +
+            </button>
+          </div>
         </div>
-        {canBeSold ? (
-          <div className="font-container"> Available </div>
-        ) : (
-          <div> Unavailable </div>
-        )}
       </div>
     );
   }
